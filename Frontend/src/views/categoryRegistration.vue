87e57cf939aaf8bb4bcfa87dae96003644919c1d
yortizher <yenifer.ortiz1@misena.edu.co>
2022-10-25 22:43:43 -0500

    Tabla 01

--- Frontend/src/views/categoryRegistration.vue
+++ Frontend/src/views/categoryRegistration.vue
@@ -1,16 +1,35 @@
 
 <script setup>
-import NuevaTabla from '../components/module-categories/NuevaTabla.vue';
+import NuevaTabla from "../components/module-categories/NuevaTabla.vue";
+import CategoriesLogic from "../logic/categories/CategoriesLogic";
+
+const arrayFrutas = CategoriesLogic;
 </script>
 
 <template>
-    <div class="container mt-5 aling-text-start">
-        <NuevaTabla/>
+  <div class="container mt-5 aling-text-center border border-dark">
+    <div class="row m-2 colors01">
+      <div class="col-md-4">
+        <h1>Registro de razas</h1>
+      </div>
+      <div class="col-md-4 offset-md-4 m-1">
+        <button type="button"  class="btn btn-success float-end"><h3 >Nuevo</h3></button>
+      </div>
     </div>
+      <CategoriesLogic />
+      <NuevaTabla
+        title1="Indice"
+        title2="Nombre"
+        title3="Opciones"
+        :arrayFrutas="arrayFrutas"
+      />
+    
+  </div>
 </template>
 
 <style scoped>
-/* .container{
-    margin-top: 400px;
+/* .container .colors01{
+  background-color: rgb(2, 2, 2);
 } */
+
 </style>
