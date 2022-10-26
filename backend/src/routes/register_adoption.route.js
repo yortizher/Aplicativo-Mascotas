be4b91f2bc3a4f8246266e9ebe8f00436a1a7cd8
drbobby27 <envioshseq@gmail.com>
2022-10-25 22:57:43 -0500

    Completing CRUD operations with Sequelize

--- backend/src/routes/register_adoption.route.js
+++ backend/src/routes/register_adoption.route.js
@@ -10,8 +10,8 @@ import {
 export const registerRouter = Router()
 
 
-registerRouter.get('/api/v1/adopting', adopting)
-registerRouter.get('/api/v1/adopting/:id', adoptingById)
-registerRouter.post('/api/v1/adopting', createAdoptions)
-registerRouter.put('/api/v1/adopting/:id', editAdoptions)
-registerRouter.delete('/api/v1/adopting/:id', deleteAdoptions)
+registerRouter.get('/api/v1/register', adopting)
+registerRouter.get('/api/v1/register/:id', adoptingById)
+registerRouter.post('/api/v1/register', createAdoptions)
+registerRouter.put('/api/v1/register/:id', editAdoptions)
+registerRouter.delete('/api/v1/register/:id', deleteAdoptions)
