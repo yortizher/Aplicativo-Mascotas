1d0373b688183e37b34377f43dfaad2e8af4d92d
drbobby27 <envioshseq@gmail.com>
2022-10-27 12:00:58 -0500

    Get images from cloudinary

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,24 @@
-import express from "express";
-import morgan from "morgan";
+import express from "express"
+import morgan from "morgan"
 import cors from 'cors'
 import { raceRouter } from './routes/race.route.js'
-
 import { speciesRouter } from './routes/species.route.js'
-
-
-
-
 import { personRefugeRouter } from './routes/person_to_refuge.route.js'
 import { refugePersonRouter } from './routes/refuge_to_person.route.js'
+import { imageRouter } from "./routes/image.route.js"
+import { uploadRouter } from "./routes/upload.route.js"
 
-const app = express();
+const app = express()
 
 app.use(cors())
 app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(raceRouter)
 app.use(speciesRouter)
-
 app.use(personRefugeRouter)
 app.use(refugePersonRouter)
+app.use(imageRouter)
+app.use(uploadRouter)
 
-export default app;
+export default app
