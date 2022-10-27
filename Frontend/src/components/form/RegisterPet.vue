a384654994970b45e3debece05220117634af1c6
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-10-27 00:23:49 -0500

    design form commit

--- Frontend/src/components/form/RegisterPet.vue
+++ Frontend/src/components/form/RegisterPet.vue
@@ -1,51 +1,66 @@
+<script setup>
+import { reactive, ref } from "@vue/reactivity"
+
+ let race = reactive(["schnauzer","Doderman", "West Highland"])
+ let category = reactive(["Categoria1","Categoria2", "Categoria3"])
+ let gender = reactive(["Hembra", "Macho"])
+ let namePet = ref("") 
+ let nameOwner = ref("")
+ let description = ref("")
+ let age = ref(0)
+ let vaccines = ref("")
+ let optionRace = ref("")
+ let optionGender = ref("")
+ let optionCategory = ref("")
+</script>
 <template>
-    <form class="container w-80">
+    <form class="container w-80" @submit.prevent="handleSubmit">
         <div class="row mt-3 mb-2 mx-2">
              <div class="col mb-3">
                 <label for="exampleInput" class="form-label">Nombre Mascota</label>
-                <input type="text" class="form-control" id="exampleInput" aria-describedby="exampleHelp">
+                <input type="text" class="form-control" id="exampleInput" aria-describedby="exampleHelp" v-model="namePet">
             </div>
             <div class="col mb-3">
                 <label for="exampleInput2" class="form-label">Nombre Dueño</label>
-                <input type="text" class="form-control" id="exampleInput2">
+                <input type="text" class="form-control" id="exampleInput2" v-model="nameOwner">
             </div>
         </div>
         <div class="row mt-3 mb-2 mx-2">
             <div class="col mb-3">
-                <label for="exampleInput2" class="form-label">Edad</label>
-                <input type="text" class="form-control" id="exampleInput2">
+                <label for="age" class="form-label">Edad</label>
+                <input type="text" class="form-control" id="age" v-model="age">
             </div>
             <div class="col mb-3">
-                <label for="exampleInput2" class="form-label">Vacunas</label>
-                <input type="text" class="form-control" id="exampleInput2">
+                <label for="vaccines" class="form-label">Vacunas</label>
+                <input type="text" class="form-control" id="vaccines" v-model="vaccines">
             </div>
         </div>
         <div class="row mt-3 mb-2 mx-2">
             <div class="col mb-3">
-                <label for="exampleInput2" class="form-label">Raza</label>
-                <select  name="seleccionProducto" id="seleccionProducto" class="form-select  text-center">
-                    <option>Seleccione</option>
+                <label for="race" class="form-label">Raza</label>
+                <select v-model="optionRace"  name="seleccionRace" id="seleccionRace" class="form-select text-center">
+                    <option v-for="(item,index) in race" v-text="item"></option>
                 </select>
             </div>
             <div class="col mb-3">
                 <label for="exampleInput2" class="form-label">Categoría</label>
-                <select  name="seleccionProducto" id="seleccionProducto" class="form-select  text-center">
-                    <option>Seleccione</option>
+                <select  v-model="optionCategory" name="seleccionCategory" id="seleccionCategory" class="form-select  text-center">
+                    <option v-for="(item,index) in category" v-text="item"></option>
                 </select>
             </div>
         </div>
         <div class="row mt-3 mb-2 mx-2">
             <div class="col mb-3">
                 <label for="exampleInput2" class="form-label">Género</label>
-                <select  name="seleccionProducto" id="seleccionProducto" class="form-select  text-center">
-                    <option>Seleccione</option>
+                <select  v-model="optionGender" name="seleccionGender" id="seleccionGender" class="form-select  text-center">
+                    <option v-for="(item,index) in gender" v-text="item"></option>
                 </select>
             </div>
         </div>
          <div class="row mt-3 mb-2 mx-2">
             <div class="col mb-3">
-                <label for="exampleInput2" class="form-label">Descripción</label>
-                <textarea  id="exampleInput2" class="form-control" name="w3review" rows="4" cols="50">
+                <label for="Description" class="form-label">Descripción</label>
+                <textarea  id="Description" class="form-control" name="w3review" rows="4" cols="50" v-model="description">
                 
                 </textarea>
             </div>
--- Frontend/src/views/breedRegistry.vue
+++ Frontend/src/views/breedRegistry.vue
@@ -1,6 +1,6 @@
 <script setup>
 import  Tabla from '../components/module-categories/Tabla.vue';
-import DBRace from '../logic/Race/DBBRace'
+import DBRace from '../logic/race/DBBRace'
 
 
 const dataPet = DBRace 
@@ -14,16 +14,14 @@ const dataPet = DBRace
                 <h1>Registro de razas</h1>
             </div>
             <div class="col-md-4 offset-md-4 m-1">
-               
                 <button type="button"  class="btn btn-success float-end" data-bs-target="#exampleModal">Nuevo</button>
-            </div>
-            
+            </div>      
         </div>
         <Tabla
-            title2="Nombre"
-            title3="Opciones"
-            :dataPet="dataPet"
-        />
+        title2="Nombre"
+        title3="Opciones"
+        :dataPet="dataPet"
+      />
     </div>
   
 </template>
